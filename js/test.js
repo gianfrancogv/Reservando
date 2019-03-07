b9d978838b855adb66342ccfc633e81c0b665c07
Gian Franco <gianfrancogv@gmail.com>
2019-03-07 00:45:25 -0300

    Paso 1.3: Testeando obtenerPuntuacion()

--- js/test.js
+++ js/test.js
@@ -1,21 +1,39 @@
 var expect = chai.expect;
+var test = listadoDeRestaurantes[0];
 
-describe("Reservando horario", function(){
+describe("Reservando horario:", function(){
 
     it("El arreglo se mantiene igual, porque el restaurant no posee el horario", function(){
-        listadoDeRestaurantes[0].reservarHorario("14:00");
-        expect(listadoDeRestaurantes[0].horarios.length).to.eql(3);
+        test.reservarHorario("14:00");
+        expect(test.horarios.length).to.eql(3);
     });
 
     it("El arreglo se mantiene igual, porque no se le pasa parámetro a la función", function(){
-        listadoDeRestaurantes[0].reservarHorario();
-        expect(listadoDeRestaurantes[0].horarios.length).to.eql(3);
+        test.reservarHorario();
+        expect(test.horarios.length).to.eql(3);
     });
     
     it("El horario correspondiente se elimina del arreglo", function(){
-        listadoDeRestaurantes[0].reservarHorario("13:00");
-        expect(listadoDeRestaurantes[0].horarios).to.eql(["15:30", "18:00"]);
+        test.reservarHorario("13:00");
+        expect(test.horarios).to.eql(["15:30", "18:00"]);
     });
 
+});
+
+describe("Obtener puntuación:", function(){
+
+    it("Puntuación calculada correctamente", function(){
+        var sumatoriaCalificaciones = 0;
+        for (var i = 0; i < test.calificaciones.length; i++) {
+            sumatoriaCalificaciones += test.calificaciones[i];
+        }
+        expect(test.obtenerPuntuacion()).to.eql(sumatoriaCalificaciones/test.calificaciones.length);
+    });
+
+    it("Puntuación igual a cero", function(){
+        if (test.calificaciones.length == 0){
+           expect(test.obtenerPuntuacion()).to.be.eql(0);
+        }
+    });
 
 });
