747bca73edfbd697e52ef651c1c5f0d01a44a2cb
Gian Franco <gianfrancogv@gmail.com>
2019-03-18 20:58:37 -0300

    Paso 2.6: aplicando find()

--- js/listado.js
+++ js/listado.js
@@ -18,12 +18,9 @@ Listado.prototype.calificarRestaurant = function(id, calificacion) {
 
 //Dado un id, busca el objeto del listado que tiene ese id
 Listado.prototype.buscarRestaurante = function(id) {
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        if (this.restaurantes[i].id === id) {
-            return this.restaurantes[i]
-        }
-    }
-    return "No se ha encontrado ningún restaurant";
+    return this.restaurantes.find(function(restaurante){
+        return restaurante.id === id;
+    });
 }
 
 //Obtiene todas las ciudades de los restaurantes sin repetidos
