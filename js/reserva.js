3b9c37e6f9b876252fef54302e5c67c4d93e12b1
Gian Franco <gianfrancogv@gmail.com>
2019-03-27 20:20:40 -0300

    Paso 3.3 Primer refactoring del proyecto terminado

--- js/reserva.js
+++ js/reserva.js
@@ -5,60 +5,64 @@ var Reserva = function(horario, cantidadPersonas, precioPorPersona, codigoDescue
     this.codigoDescuento = codigoDescuento;
 }
 
+// Obteniendo precio base:
 Reserva.prototype.obtenerPrecioBase = function() {
     return this.cantidadPersonas * this.precioPorPersona;    
 }
 
+// Descuentos:
 Reserva.prototype.descuentosCantidadPersonas = function() {
-    var descuento = 0;
     if (this.cantidadPersonas >= 4 && this.cantidadPersonas <= 6) {
-        descuento = this.obtenerPrecioBase() * 0.05;
+        return this.obtenerPrecioBase() * 0.05;
     } else if (this.cantidadPersonas >= 7 && this.cantidadPersonas <= 8) {
-        descuento  = this.obtenerPrecioBase() * 0.1;
+        return this.obtenerPrecioBase() * 0.1;
     } else if (this.cantidadPersonas > 8) {
-        descuento = this.obtenerPrecioBase() * 0.15;
+        return this.obtenerPrecioBase() * 0.15;
+    } else {
+        return 0;
     }
-    return descuento;
 }
 
 Reserva.prototype.descuentosPorCodigo = function() {
-    var descuento = 0;
     if (this.codigoDescuento === 'DES15') {
-        descuento = this.obtenerPrecioBase() * 0.15;
+        return this.obtenerPrecioBase() * 0.15;
     } else if (this.codigoDescuento === 'DES200') {
-        descuento = 200;
+        return 200;
     } else if (this.codigoDescuento === 'DES1') {
-        descuento = this.precioPorPersona;
+        return this.precioPorPersona;
+    } else {
+        return 0;
     }
-    return descuento;
-}
-
-Reserva.prototype.adicionalPorHora = function() {
-    var adicional = 0;
-    var hora = this.horario.getHours();
-    if (hora >= 13 && hora <= 14 || hora >= 20 && hora <= 21) {
-        adicional = this.obtenerPrecioBase() * 0.05;
-    }
-    return adicional;
-}
-
-Reserva.prototype.adicionalPorDia = function() {
-    var adicional = 0;
-    var dia = this.horario.getDay();
-    if (dia >= 5 && dia <= 7) {
-        adicional = this.obtenerPrecioBase() * 0.1;
-    }
-    return adicional;
 }
 
 Reserva.prototype.descuentos = function() {
     return this.descuentosCantidadPersonas() + this.descuentosPorCodigo();
 }
 
+// Adicionales:
+Reserva.prototype.adicionalPorHora = function() {
+    var hora = this.horario.getHours();
+    if (hora >= 13 && hora <= 14 || hora >= 20 && hora <= 21) {
+        return this.obtenerPrecioBase() * 0.05;
+    } else {
+        return 0;
+    }
+}
+
+Reserva.prototype.adicionalPorDia = function() {
+    var dia = this.horario.getDay();
+    if (dia >= 5 && dia <= 7) {
+        return this.obtenerPrecioBase() * 0.1;
+    } else {
+        return 0;
+    }
+}
+
 Reserva.prototype.adicionales = function() {
     return this.adicionalPorDia() + this.adicionalPorHora();
 }
 
+// Obteniendo precio final:
 Reserva.prototype.obtenerPrecioFinal = function(){
     return this.obtenerPrecioBase() + this.adicionales() - this.descuentos();
 }
