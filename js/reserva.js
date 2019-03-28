47c569fb880489a73067ed31b48923021033dd3a
Gian Franco <gianfrancogv@gmail.com>
2019-03-27 21:40:28 -0300

    Paso 3.3: Refactor finalizado

--- js/reserva.js
+++ js/reserva.js
@@ -7,62 +7,70 @@ var Reserva = function(horario, cantidadPersonas, precioPorPersona, codigoDescue
 
 // Obteniendo precio base:
 Reserva.prototype.obtenerPrecioBase = function() {
-    return this.cantidadPersonas * this.precioPorPersona;    
+    var precioBase = 0;
+    precioBase = this.cantidadPersonas * this.precioPorPersona;
+    return precioBase;
 }
 
 // Descuentos:
 Reserva.prototype.descuentosCantidadPersonas = function() {
+    var descuento = 0;
     if (this.cantidadPersonas >= 4 && this.cantidadPersonas <= 6) {
-        return this.obtenerPrecioBase() * 0.05;
+        descuento = this.obtenerPrecioBase() * 0.05;
     } else if (this.cantidadPersonas >= 7 && this.cantidadPersonas <= 8) {
-        return this.obtenerPrecioBase() * 0.1;
+        descuento = this.obtenerPrecioBase() * 0.1;
     } else if (this.cantidadPersonas > 8) {
-        return this.obtenerPrecioBase() * 0.15;
-    } else {
-        return 0;
+        descuento = this.obtenerPrecioBase() * 0.15;
     }
+    return descuento;
 }
 
 Reserva.prototype.descuentosPorCodigo = function() {
-    if (this.codigoDescuento === 'DES15') {
-        return this.obtenerPrecioBase() * 0.15;
-    } else if (this.codigoDescuento === 'DES200') {
-        return 200;
-    } else if (this.codigoDescuento === 'DES1') {
-        return this.precioPorPersona;
-    } else {
-        return 0;
+    var descuento = 0;
+    if (this.codigoDescuento == 'DES15') {
+        descuento = this.obtenerPrecioBase() * 0.15;
+    } else if (this.codigoDescuento == 'DES200') {
+        descuento = 200;
+    } else if (this.codigoDescuento == 'DES1') {
+        descuento = this.precioPorPersona;
     }
+    return descuento; 
 }
 
 Reserva.prototype.descuentos = function() {
-    return this.descuentosCantidadPersonas() + this.descuentosPorCodigo();
+    var descuento;
+    descuento = this.descuentosCantidadPersonas() + this.descuentosPorCodigo();
+    return descuento;
 }
 
 // Adicionales:
 Reserva.prototype.adicionalPorHora = function() {
+    var adicional = 0;
     var hora = this.horario.getHours();
     if (hora >= 13 && hora <= 14 || hora >= 20 && hora <= 21) {
-        return this.obtenerPrecioBase() * 0.05;
-    } else {
-        return 0;
-    }
+        adicional = this.obtenerPrecioBase() * 0.05;
+    } 
+    return adicional;
 }
 
 Reserva.prototype.adicionalPorDia = function() {
+    var adicional = 0;
     var dia = this.horario.getDay();
     if (dia >= 5 && dia <= 7) {
-        return this.obtenerPrecioBase() * 0.1;
-    } else {
-        return 0;
-    }
+        adicional = this.obtenerPrecioBase() * 0.1;
+    } 
+    return adicional;
 }
 
 Reserva.prototype.adicionales = function() {
-    return this.adicionalPorDia() + this.adicionalPorHora();
+    var adicional;
+    adicional = this.adicionalPorDia() + this.adicionalPorHora();
+    return adicional;
 }
 
 // Obteniendo precio final:
 Reserva.prototype.obtenerPrecioFinal = function(){
-    return this.obtenerPrecioBase() + this.adicionales() - this.descuentos();
+    var precioFinal = 0;
+    precioFinal = this.obtenerPrecioBase() + this.adicionales() - this.descuentos();
+    return precioFinal;
 }
--- js/test.js
+++ js/test.js
@@ -3,17 +3,17 @@ var test = listadoDeRestaurantes[0];
 
 describe("Reservando horario:", function(){
 
-    it("El arreglo se mantiene igual, porque el restaurant no posee el horario", function(){
+    it("El arreglo de horarios se mantiene igual porque el restaurant no posee el horario", function(){
         test.reservarHorario("14:00");
         expect(test.horarios.length).to.eql(3);
     });
 
-    it("El arreglo se mantiene igual, porque no se le pasa parámetro a la función", function(){
+    it("El arreglo de horarios se mantiene igual porque no se le pasa parámetro a la función", function(){
         test.reservarHorario();
         expect(test.horarios.length).to.eql(3);
     });
     
-    it("El horario correspondiente se elimina del arreglo", function(){
+    it("El horario correspondiente se elimina del arreglo de horarios porque cumple las condiciones", function(){
         test.reservarHorario("13:00");
         expect(test.horarios).to.eql(["15:30", "18:00"]);
     });
@@ -22,7 +22,7 @@ describe("Reservando horario:", function(){
 
 describe("Obteniendo puntuación:", function(){
 
-    it("Puntuación calculada correctamente", function(){
+    it("La puntuación es calculada correctamente", function(){
         var sumatoriaCalificaciones = 0;
         for (var i = 0; i < test.calificaciones.length; i++) {
             sumatoriaCalificaciones += test.calificaciones[i];
@@ -30,7 +30,7 @@ describe("Obteniendo puntuación:", function(){
         expect(test.obtenerPuntuacion()).to.eql(sumatoriaCalificaciones/test.calificaciones.length);
     });
 
-    it("Puntuación igual a cero", function(){
+    it("La puntuación es igual a cero ya que no tiene ninguna calificación", function(){
         if (test.calificaciones.length == 0){
            expect(test.obtenerPuntuacion()).to.be.eql(0);
         }
@@ -38,16 +38,16 @@ describe("Obteniendo puntuación:", function(){
 
 });
 
-describe("Calificando restaurant:", function(){
+describe("Función calificar():", function(){
 
-    it("Se agrega una nueva calificación al restaurant", function(){
+    it("Se agrega una nueva calificación al restaurant correctamente", function(){
         test.calificar(9);
         expect(test.calificaciones[test.calificaciones.length - 1]).to.eql(9);
     });
 
 });
 
-describe("Buscando restaurant por su id:", function(){
+describe("Función buscarRestaurante(id):", function(){
 
     it("Comparando el listado de restaurantes con la búsqueda mediante el id", function(){
         expect(listado.buscarRestaurante(1)).to.eql(test);
@@ -57,30 +57,34 @@ describe("Buscando restaurant por su id:", function(){
 
 describe("Obteniendo restaurant según filtro aplicado:", function(){
 
-    it("Filtrando un restaurant con tres parámetros, esperando sea igual al restaurant elegido", function(){
+    it("Filtrando un restaurant con tres parámetros, esperando sea igual al restaurant elegido utilizando filter()", function(){
         expect(listado.obtenerRestaurantes("Asiática","Nueva York","18:00")[0]).to.eql(test);
     });
 
 });
 
-describe("Reservas", function(){
+describe("Test de reservas: Reserva 1", function(){
 
-    it("El precio base debería ser igual a la cantidad de personas multiplicado por el precio por persona", function(){
+    it("El precio base es igual a la cantidad de personas multiplicado por el precio por persona", function(){
         var reserva1 = new Reserva (new Date(2018, 7, 24, 11, 00), 8, 350, "DES1");
         expect(reserva1.obtenerPrecioBase()).to.eql(2800);
     });
 
-    it("El precio final debería ser igual al precio base más adicionales menos descuentos", function(){
+    it("El precio final es igual al precio base más adicionales menos descuentos", function(){
         var reserva1 = new Reserva (new Date(2018, 7, 24, 11, 00), 8, 350, "DES1");
         expect(reserva1.obtenerPrecioFinal()).to.eql(2450);
     });
 
-    it("El precio base debería ser igual a la cantidad de personas multiplicado por el precio por persona", function(){
+});
+
+describe("Test de reservas: Reserva 2", function(){
+
+    it("El precio base es igual a la cantidad de personas multiplicado por el precio por persona", function(){
         var reserva2 = new Reserva (new Date(2018, 7, 27, 14, 100), 2, 150, "DES200");
         expect(reserva2.obtenerPrecioBase()).to.eql(300);
     });
-    
-    it("El precio final debería ser igual al precio base más adicionales menos descuentos", function(){
+
+    it("El precio final es igual al precio base más adicionales menos descuentos", function(){
         var reserva2 = new Reserva (new Date(2018, 7, 27, 14, 100), 2, 150, "DES200");
         expect(reserva2.obtenerPrecioFinal()).to.eql(100);
     });
